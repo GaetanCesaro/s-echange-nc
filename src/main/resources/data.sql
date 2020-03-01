7cbca6bd11e12fe5b9fe487f11ecb65919a4a17a
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2020-03-01 15:12:50 +1100

    chore: Post object and datas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,26 @@
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+CREATE TABLE user (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   email VARCHAR(250) NOT NULL,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
+  firstname VARCHAR(250) NOT NULL,
+  lastname VARCHAR(250) NOT NULL,
 );
 
-INSERT INTO users (email, first_name, last_name) VALUES
+CREATE TABLE post (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  title VARCHAR(250) NOT NULL,
+  description VARCHAR(250) NOT NULL,
+  price INT NOT NULL,
+  owner INT NOT NULL,
+);
+
+INSERT INTO user (email, first_name, last_name) VALUES
   ('dangite.aliko@gmail.com', 'Aliko', 'Dangote'),
   ('bill.gates@hotmail.com', 'Bill', 'Gates'),
   ('folrunsho.alokija@yahoo.com', 'Folrunsho', 'Alakija');
+
+INSERT INTO post (title, description) VALUES
+  ('Lit bébé', 'Peu servi, acheté chez bébé neuf', 20000, 1),
+  ('Guitare acoustique Yamaha', 'Bon état, vendue car nouvelle guitare offerte par ma compagne', 15000, 1),
+  ('Ventilateur de ouf', 'Il fait chaud, un bon ventilateur c''est éssentiel !', 5900, 2);

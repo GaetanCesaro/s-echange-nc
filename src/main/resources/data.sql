119ab6ec6f4ef7fb6d7ee3f143347a10d9f7999e
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2020-03-01 15:37:07 +1100

    fix: Database attributes with _ when CamelCasingAttribute

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS user;
 CREATE TABLE user (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   email VARCHAR(250) NOT NULL,
-  firstname VARCHAR(250) NOT NULL,
-  lastname VARCHAR(250) NOT NULL,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL,
 );
 
 DROP TABLE IF EXISTS post;
@@ -12,15 +12,15 @@ CREATE TABLE post (
   title VARCHAR(250) NOT NULL,
   description VARCHAR(250) NOT NULL,
   price INT NOT NULL,
-  owner INT NOT NULL,
+  owner_id INT NOT NULL,
 );
 
-INSERT INTO user (email, firstname, lastname) VALUES
+INSERT INTO user (email, first_name, last_name) VALUES
   ('dangite.aliko@gmail.com', 'Aliko', 'Dangote'),
   ('bill.gates@hotmail.com', 'Bill', 'Gates'),
   ('folrunsho.alokija@yahoo.com', 'Folrunsho', 'Alakija');
 
-INSERT INTO post (title, description, price, owner) VALUES
+INSERT INTO post (title, description, price, owner_id) VALUES
   ('Lit bébé', 'Peu servi, acheté chez bébé neuf', 20000, 1),
   ('Guitare acoustique Yamaha', 'Bon état, vendue car nouvelle guitare offerte par ma compagne', 15000, 1),
   ('Ventilateur de ouf', 'Il fait chaud, un bon ventilateur c''est éssentiel !', 5900, 2);

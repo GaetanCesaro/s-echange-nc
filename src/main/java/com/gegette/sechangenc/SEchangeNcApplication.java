4d3f722d4c989a672d1ec8391c099624cfdeaa56
Gaëtan CESARO <gaetan.cesaro@gmail.com>
2020-03-01 11:34:09 +1100

    Spring initializr

---
+++ src/main/java/com/gegette/sechangenc/SEchangeNcApplication.java
@@ -0,0 +1,13 @@
+package com.gegette.sechangenc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SEchangeNcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SEchangeNcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gegette/sechangenc/SEchangeNcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gegette.sechangenc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SEchangeNcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
